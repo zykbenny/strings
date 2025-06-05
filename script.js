e1cc5e787188f93f89990adb4d56d27dd7336b36
benny <bianca.graczyk.lima@escola.pr.gov.br>
2025-06-05 11:44:01 +0000

    começando

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    alert('Clicado fui!');
+}
