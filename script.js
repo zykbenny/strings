8f6fc65a75e07191687480a164c0369e00d0c842
benny <bianca.graczyk.lima@escola.pr.gov.br>
2025-06-12 11:41:24 +0000

    aula5 alura

--- script.js
+++ script.js
@@ -14,5 +14,19 @@ const palavrasChave = processaTexto(texto);
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
 
+  let frequencias = {};
+
+  for (let i of palavras) {
+    frequencias[i] = 0;
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
+    }
+  }
+
+  console.log(frequencias);
+
   return palavras;
 }
