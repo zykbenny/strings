d1aac07d22e8aad78ffedf5a27fea05c0774aa41
benny <bianca.graczyk.lima@escola.pr.gov.br>
2025-07-01 22:35:30 +0000

    aula6 alura

--- script.js
+++ script.js
@@ -13,9 +13,18 @@ const palavrasChave = processaTexto(texto);
 }
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
+  const frequencias = contaFrequencias(palavras);
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+}
+console.log(ordenadas);
+
+  return ordenadas.slice(0, 10);
+}
+function contaFrequencias(palavras) {
   let frequencias = {};
-
   for (let i of palavras) {
     frequencias[i] = 0;
 
@@ -25,8 +34,6 @@ function processaTexto(texto) {
       }
     }
   }
+      return frequencias;
 
-  console.log(frequencias);
-
-  return palavras;
 }
